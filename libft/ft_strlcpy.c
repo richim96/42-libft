64753e590ea6e7b5f84e3a00feb1fb6ab59a94d9
Riccardo Mei <rmei@c4a12c5.42berlin.de>
2024-04-29 20:27:23 +0200

    update project

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,25 +6,28 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 16:56:41 by rmei              #+#    #+#             */
-/*   Updated: 2024/04/25 18:12:17 by rmei             ###   ########.fr       */
+/*   Updated: 2024/04/29 20:03:35 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/* Copies up to size - 1 bytes from src to dest, null-terminating the result.
- * Returns the total length of the string it tried to create (src). */
+/* Copies up to (dest) size - 1 bytes from src to dest, null-terminating the
+ * result. Returns the total length of the string it tried to create (src). */
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	src_len;
 
+	src_len = ft_strlen(src);
+	if (size <= 0)
+		return (src_len);
 	i = 0;
-	while (size > 0 && src[i] && i < size - 1)
+	while (src[i] && i < size - 1)
 	{
 		dst[i] = src[i];
 		i++;
 	}
-	if (size > 0)
-		dst[i] = '\0';
-	return (ft_strlen(src));
+	dst[i] = '\0';
+	return (src_len);
 }
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,16 +6,32 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 13:42:44 by rmei              #+#    #+#             */
-/*   Updated: 2024/04/27 14:13:33 by rmei             ###   ########.fr       */
+/*   Updated: 2024/04/29 20:10:54 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
 
 /* Creates a substring from a given string, copying from to start up to len.
- * Return a pointer to the new string, or NULL if the allocation fails. */
+ * Returns a pointer to the new string, or NULL if the allocation fails. */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	s_len;
+	size_t	buffer_size;
+	char	*substring;
 
+	s_len = ft_strlen(s);
+	if (start > s_len)
+	{
+		start = 0;
+		len = 0;
+	}
+	else if (len + start > s_len)
+		len = s_len - start;
+	buffer_size = len + 1;
+	substring = ft_calloc(buffer_size, sizeof(char));
+	if (!substring)
+		return (NULL);
+	ft_strlcpy(substring, s + start, buffer_size);
+	return (substring);
 }

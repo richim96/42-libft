fa11795a811f29579e1bfcb599edc86a0853d065
Riccardo Mei <rmei@c4a12c5.42berlin.de>
2024-05-10 16:36:51 +0200

    update error

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:33:40 by rmei              #+#    #+#             */
-/*   Updated: 2024/04/26 16:44:41 by rmei             ###   ########.fr       */
+/*   Updated: 2024/05/10 16:36:39 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	if (!src && !dest)
-		return (NULL);
 	i = 0;
 	while (i < n)
 	{

be8a13bbf5eebf8061d5068e0fe3fb52d6820db4
Riccardo Mei <rmei@c4a12c5.42berlin.de>
2024-05-17 16:39:10 +0200

    complete mandatory printf

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,82 +6,67 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 11:28:04 by rmei              #+#    #+#             */
-/*   Updated: 2024/05/17 13:00:39 by rmei             ###   ########.fr       */
+/*   Updated: 2024/05/17 16:35:07 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-
-#include <stdarg.h>
-#include <stdlib.h>
 #include "ft_printf.h"
 
-//static char	*g_flags;
-
-//g_flags = "-0.# +";
-
-//static void	ft_set_flags(char *s);
-//static char	*ft_print_flags(char *s, char *s_var);
-
 /* Prints to screen the current variable, returns the n_chars printed */
-static int ft_print_arg(const char *fmt, va_list args)
+static int	ft_print_arg(const char *fmt, va_list args, void *ptr)
 {
-	char	*var;
+	char	*hex_base;
+	char	*hex_base_cap;
 
+	hex_base = "0123456789abcdef";
+	hex_base_cap = "0123456789ABCDEF";
 	if (*fmt == 'c')
-		return (ft_putchar_fd(va_arg(args, int), 1));
+		return (ft_putchar(va_arg(args, int)));
 	else if (*fmt == 's')
-		return (ft_putstr_fd(va_arg(args, char *), 1));
+		return (ft_putstr(va_arg(args, char *)));
+	else if (*fmt == 'p')
+	{
+		ptr = va_arg(args, void *);
+		if (!ptr)
+			return (ft_putstr("(nil)"));
+		return (ft_putstr("0x") + ft_putnbr_base((unsigned long)ptr, hex_base));
+	}
 	else if (*fmt == 'd' || *fmt == 'i')
-		return (ft_putnbr_fd(va_arg(args, int), 1));
+		return (ft_putnbr(va_arg(args, int)));
 	else if (*fmt == 'u')
-		return (ft_putnbr_fd(va_arg(args, unsigned int), 1));
+		return (ft_putnbr(va_arg(args, unsigned int)));
 	else if (*fmt == 'x')
-		return (ft_putnbr_base_fd(va_arg(args, unsigned int), 1, "0123456789abcdef"));
+		return (ft_putnbr_base(va_arg(args, unsigned int), hex_base));
 	else if (*fmt == 'X')
-		return (ft_putnbr_base_fd(va_arg(args, unsigned int), 1, "0123456789ABCDEF"));
-	else
-		return (ft_putchar_fd(*fmt, 1));
+		return (ft_putnbr_base(va_arg(args, unsigned int), hex_base_cap));
+	return (ft_putchar(*fmt));
 }
 
-//static char	*ft_ptr_shift(char *s);
-
-/* Manages printing 'cspdiuxX%' specifiers formatted with '-0.# +' flags and
- * width. Doesn't handle buffer size. Returns the number of chars printed. */
+/* Manages printing 'cspdiuxX%' specifiers. Doesn't handle buffer size.
+ * Returns the number of chars printed. */
 int	ft_printf(const char *fmt, ...)
 {
 	int		n_chars;
-	char	*var;
 	va_list	args;
+	void	*ptr;
 
 	n_chars = 0;
+	ptr = NULL;
 	va_start(args, fmt);
 	while (*fmt)
 	{
 		while (*fmt && *fmt != '%')
 		{
-			ft_putchar_fd(*fmt, 1);
+			ft_putchar(*fmt);
 			n_chars++;
 			fmt++;
 		}
 		if (!*fmt)
 			break ;
 		fmt++;
-		n_chars += ft_print_arg(fmt, args);
+		n_chars += ft_print_arg(fmt, args, ptr);
 		fmt++;
 	}
 	va_end(args);
-	printf("My chars: %d\n", n_chars);
 	return (n_chars);
-}
-
-int	main(void)
-{
-	int	chars;
-
-	ft_printf("Hello, %d %d %x %X\n", 1000, 11, 1400, -4294967290);
-	chars = printf("Hello, %d %d %x %X\n", 1000, 11, 1400, -4294967290);
-	printf("Printf chars: %d\n", chars);
-	return 0;
 }
--- printf/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int	main(void)
-{
-	printf("%10s\n:", "hello");
-	return 0;
-}

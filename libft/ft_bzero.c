f7d586efa301fff2e6dd12310255539d85cf01ca
Riccardo Mei <rmei@Riccardos-MacBook-Pro.local>
2024-05-29 11:19:21 +0200

    update bzero

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:21:40 by rmei              #+#    #+#             */
-/*   Updated: 2024/05/10 11:45:19 by rmei             ###   ########.fr       */
+/*   Updated: 2024/05/29 11:18:34 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 /* Erases data by setting n bytes of data at memory location s to 0 */
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*ptr;
-
-	ptr = (unsigned char *)s;
 	while (n--)
-		*ptr++ = '\0';
+		*((unsigned char *)s++) = '\0';
 }

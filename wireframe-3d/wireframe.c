bb48ee69991158535d34331799e7e1172656d467
Riccardo Mei <rmei@c4a12c5.42berlin.de>
2024-07-02 18:30:59 +0200

    create first image - still no display

--- wireframe-3d/wireframe.c
+++ wireframe-3d/wireframe.c
@@ -6,20 +6,39 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 14:14:12 by rmei              #+#    #+#             */
-/*   Updated: 2024/07/02 14:14:32 by rmei             ###   ########.fr       */
+/*   Updated: 2024/07/02 18:28:54 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wireframe.h"
 
+static t_img	ft_img_make(void *mlx_ptr)
+{
+	t_img	img;
+
+	img.img_ptr = mlx_new_image(mlx_ptr, 500, 375);
+	img.img_addr = mlx_get_data_addr(
+			img.img_ptr,
+			&img.bits_per_pixel,
+			&img.size_line,
+			&img.endian
+			);
+	return (img);
+}
+
 void	ft_map_draw(char *map)
 {
+	int		fd;
+	char	*row;
 	void	*mlx_ptr;
 	void	*win_ptr;
+	t_img	img;
 
-	if (!map)
-		return ;
+	fd = open(map, O_RDONLY);
+	row = ft_get_next_line(fd);
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 350, "LANDSCAPE WIREFRAME");
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 750, map);
+	img = ft_img_make(mlx_ptr);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, img.img_ptr, 0, 0);
 	mlx_loop(mlx_ptr);
 }
--- wireframe-3d/wireframe.h
+++ wireframe-3d/wireframe.h
@@ -6,7 +6,7 @@
 /*   By: rmei <rmei@student.42berlin.de>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 20:24:40 by rmei              #+#    #+#             */
-/*   Updated: 2024/07/02 14:15:04 by rmei             ###   ########.fr       */
+/*   Updated: 2024/07/02 16:38:54 by rmei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,17 @@
 # include "libft.h"
 # include "mlx.h"
 
+/* STRUCTURES */
+typedef struct s_img
+{
+	void	*img_ptr;
+	char	*img_addr;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+}	t_img;
+
+/* FUNCTIONS  */
 int		ft_isnumstr(char *s);
 void	ft_map_draw(char *map);
 
